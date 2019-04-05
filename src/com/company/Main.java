52b0c9d9927a6206b2ad55a302f999d2bc9d607c
usama fouad <usamaeltmsah@yahoo.com>
2019-04-05 21:57:22 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        FolderStructure root = new FolderStructure("root");
-//        FolderStructure folderStructure = new FolderStructure();
-        root.createFolder("USA", root);
-        root.createFolder("USA", root);
-        root.createFolder("US", root);
-        root.deleteFolder("USA", root);
-
-        FileStructure fileStructure = new FileStructure();
-        fileStructure.createFile("file.txt", root);
-        fileStructure.createFile("file.txt", root);
-        fileStructure.createFile("file.txt", root);
-        fileStructure.deleteFile("file.txt", root);
     }
 }
